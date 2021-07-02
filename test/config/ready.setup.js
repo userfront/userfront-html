d48377ed702c9f786a3417987dd2adaa71e891b2
Tyler <tyler@anymod.com>
2021-07-02 07:23:55 -0700

    Update cdn

--- test/config/ready.setup.js
+++ test/config/ready.setup.js
@@ -5,5 +5,5 @@ export const tenantId = "demo1234";
 // prettier-ignore
 (function(m,o,d,u,l,a,r,i,z,e) {
   u[m]={rq:[],ready:function(j){u[m].rq.push(j);},m:m,o:o,d:d,r:r};u.amvartem=m;
-})("Userfront", tenantId, "https://mod.userfront.com/v3", window, document, "script");
+})("Userfront", tenantId, "https://cdn.userfront.com/toolkit", window, document, "script");
 // prettier-ignore
